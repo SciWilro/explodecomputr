629bb1888b1ebb31f02a852e6f66a50ee15c91e0
Gibran Hemani <explodecomputer@gmail.com>
2014-07-05 12:17:22 +0100

    added data reading options and qqplot

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,8 @@
+################
+# READING DATA #
+################
+
+
 #' Read binary GRM files into R
 #'
 #' @param rootname
@@ -38,6 +43,50 @@ readGRM <- function(rootname)
 }
 
 
+#' Check if files exist
+#'
+#' Either checks of all files for a rootname exist, or returns a specific suffix
+#'
+#' @param rootname
+#' @param software Either "plink" or "gcta"
+#' @param suffix Either "all" or a specific suffic
+#'
+#' @export
+#' @return Character string
+checkRootname <- function(rootname, software="plink", suffix="all")
+{
+	sfs <- list(
+		gcta = c("all", "grm.bin", "grm.N.bin", "grm.id"),
+		plink = c("all", "bim", "fam", "bed")
+	)
+
+	stopifnot(software %in% names(sfs))
+	stopifnot(suffix %in% sfs[[software]])
+
+	if(suffix=="all")
+	{
+		nom <- paste(rootname, sfs[[software]][-1], sep=".")
+		a <- file.exists(nom)
+		if(!any(a))
+		{
+			stop(paste(nom[!a], collapse=" "))
+		} else {
+			return(rootname)
+		}
+	} else {
+		if(file.exists(rootname))
+		{
+			return(rootname)
+		} else if (file.exists(paste(rootname, suffix, sep="."))) {
+			return(paste(rootname, suffix, sep="."))
+		} else {
+			stop(paste(suffix, "file does not exist"))
+		}
+	}
+}
+
+
+
 #' Read bim file
 #' 
 #' @param rootname
@@ -45,6 +94,76 @@ readGRM <- function(rootname)
 #' @return Data frame
 readBim <- function(rootname)
 {
-	nom <- ifelse(file.exists(rootname), rootname, paste(rootname, ".bim", sep=""))
-	
+	nom <- checkRootname(rootname, "plink", "bim")
+	bim <- read.table(nom, colClasses=c("character", "character", "numeric", "numeric", "character", "character"))
+	names(bim) <- c("CHR", "SNP", "GD", "BP", "A1", "A2")
+	return(bim)	
 }
+
+
+#' Read fam file
+#' 
+#' @param rootname
+#' @export
+#' @return Data frame
+readFam <- function(rootname)
+{
+	nom <- checkRootname(rootname, "plink", "fam")
+	fam <- read.table(nom, colClasses=c("character", "character", "numeric", "numeric", "character", "character"))
+	names(fam) <- c("FID", "IID", "FATHER", "MOTHER", "SEX", "PHEN")
+	return(fam)	
+}
+
+
+#' Read output from plink --linear
+#'
+#' @param filename
+#' @param h Is there a header
+#'
+#' @export
+#' @return Data.frame
+readPlinkLinear <- function(filename, h=TRUE)
+{
+	a <- read.table(filename, header=h, colClass=c("character", "character", "numeric", "character", "character", "numeric", "numeric", "numeric", "numeric"))
+	if(!h)
+	{
+		names(a) <- c("CHR", "SNP", "BP", "A1", "TEST", "NMISS", "BETA", "STAT", "P")
+	}
+	return(a)
+}
+
+a <- readPlinkLinear("aar0.txt.assoc.linear.add.gz", FALSE)
+
+
+
+##########
+# GRAPHS #
+##########
+
+
+
+#' <brief desc>
+#'
+#' <full description>
+#'
+#' @param gwas <what param does>
+#' @param  filename=NULL <what param does>
+#'
+#' @export
+#' @return
+qqplotpval <- function(gwas, filename=NULL)
+{
+	require(GenABEL)
+	l <- estlambda(gwas$P, method="median")
+	nom <- paste("lambda = ", round(l$estimate, 3), sep="")
+	if(!is.null(filename))
+	{
+		png(filename)
+	}
+	estlambda(gwas$P, method="median", plot=TRUE, main=nom)
+	if(!is.null(filename))
+	{
+		dev.off()
+	}
+}
+
